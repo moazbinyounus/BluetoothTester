e85029ab4a58cc66a5084532a6439218afd0cfbe
moaz <moazby@gmail.com>
2022-12-19 10:17:10 +0500

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/moaz/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/moaz/Storage/Flutter_Projects/FunctionGenerator/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/moaz/Storage/Flutter_Projects/FunctionGenerator/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.4.0"
+export "FLUTTER_BUILD_NUMBER=0.4.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/moaz/Storage/Flutter_Projects/FunctionGenerator/example/.dart_tool/package_config.json"
